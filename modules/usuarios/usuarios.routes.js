9a37f85009c9448d4a0ae17e7512192860024988
angelogds <126365784+angelogds@users.noreply.github.com>
2026-02-09 19:34:17 -0300

    Update usuarios.routes.js

--- modules/usuarios/usuarios.routes.js
+++ modules/usuarios/usuarios.routes.js
@@ -1,17 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { requireLogin, requireRole } = require("../auth/auth.middleware");
-const controller = require("./usuarios.controller");
+const { requireRole } = require("../auth/auth.middleware");
+const ctrl = require("./usuarios.controller");
 
-// tudo em /admin/users
-router.get("/admin/users", requireLogin, requireRole(["ADMIN"]), controller.list);
-router.get("/admin/users/new", requireLogin, requireRole(["ADMIN"]), controller.newForm);
-router.post("/admin/users", requireLogin, requireRole(["ADMIN"]), controller.create);
-
-router.get("/admin/users/:id/edit", requireLogin, requireRole(["ADMIN"]), controller.editForm);
-router.post("/admin/users/:id", requireLogin, requireRole(["ADMIN"]), controller.update);
-
-router.post("/admin/users/:id/reset-password", requireLogin, requireRole(["ADMIN"]), controller.resetPassword);
+router.get("/usuarios", requireRole(["admin"]), ctrl.userIndex);
+router.get("/usuarios/novo", requireRole(["admin"]), ctrl.userNewForm);
+router.post("/usuarios", requireRole(["admin"]), ctrl.userCreate);
 
 module.exports = router;
