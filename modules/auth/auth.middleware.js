4cd5f2baea98f926add76fdaeb22720b57844f30
angelogds <126365784+angelogds@users.noreply.github.com>
2026-02-09 06:43:42 -0300

    Update auth.middleware.js

--- modules/auth/auth.middleware.js
+++ modules/auth/auth.middleware.js
@@ -1,1 +1,24 @@
-exports.requireLogin=(req,res,next)=>req.session.user?next():res.redirect('/login');
+const { hasPermission } = require('../../utils/security/permissions');
+
+function requireLogin(req, res, next) {
+  if (!req.session.user) {
+    return res.redirect('/login');
+  }
+  next();
+}
+
+function requireRole(permission) {
+  return (req, res, next) => {
+    if (!req.session.user) {
+      return res.redirect('/login');
+    }
+
+    if (!hasPermission(req.session.user.role, permission)) {
+      return res.status(403).send('Acesso negado');
+    }
+
+    next();
+  };
+}
+
+module.exports = { requireLogin, requireRole };
