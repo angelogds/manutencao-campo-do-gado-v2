9c99a6c762236e0879e3c834e567addf2828fca5
angelogds <126365784+angelogds@users.noreply.github.com>
2026-02-09 06:58:08 -0300

    Update auth.routes.js

--- modules/auth/auth.routes.js
+++ modules/auth/auth.routes.js
@@ -1,9 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const controller = require('./auth.controller');
 
-router.get('/login', controller.loginForm);
-router.post('/login', controller.login);
-router.get('/logout', controller.logout);
+const { showLogin, doLogin, doLogout } = require("./auth.controller");
+
+router.get("/login", showLogin);
+router.post("/login", doLogin);
+router.get("/logout", doLogout);
 
 module.exports = router;
