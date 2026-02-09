8b33d4acf183a6628fc4de1ecd3046713290c67c
angelogds <126365784+angelogds@users.noreply.github.com>
2026-02-09 08:55:57 -0300

    Update os.routes.js

--- modules/os/os.routes.js
+++ modules/os/os.routes.js
@@ -1,6 +1,14 @@
+const express = require("express");
+const router = express.Router();
 
-const r=require('express').Router(); const db=require('../../database/db');
-r.get('/',(q,s)=>s.render('os/index',{rows:db.prepare('SELECT * FROM os').all()}));
-r.get('/nova',(q,s)=>s.render('os/nova'));
-r.post('/nova',(q,s)=>{db.prepare('INSERT INTO os(equipamento,descricao,tipo,status,opened_by,opened_at) VALUES(?,?,?,?,?,datetime(\'now\'))').run(q.body.equipamento,q.body.descricao,q.body.tipo,'ABERTA',q.session.user.id); s.redirect('/os');});
-module.exports=r;
+const { requireLogin, requireRole } = require("../auth/auth.middleware");
+const { ROLES } = require("../../utils/security/permissions");
+
+router.get(
+  "/os",
+  requireLogin,
+  requireRole([ROLES.ADMIN, ROLES.MANUTENCAO, ROLES.RH]),
+  (req, res) => res.render("os/index", { title: "Ordens de Serviço" })
+);
+
+module.exports = router;
