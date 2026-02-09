72ca318e04aa51926facc5b3cca3210060802e32
angelogds <126365784+angelogds@users.noreply.github.com>
2026-02-09 10:09:33 -0300

    Update os.routes.js

--- modules/os/os.routes.js
+++ modules/os/os.routes.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-const { requireLogin } = require("../auth/auth.middleware");
-const ctrl = require("./os.controller");
+const { requireLogin, requireRole } = require("../auth/auth.middleware");
+const controller = require("./usuarios.controller");
 
-router.get("/os", requireLogin, ctrl.index);
-router.get("/os/nova", requireLogin, ctrl.createForm);
-router.post("/os", requireLogin, ctrl.create);
+// tudo em /admin/users (ADMIN)
+router.use("/admin/users", requireLogin, requireRole(["ADMIN"]));
+
+router.get("/admin/users", controller.list);
+router.get("/admin/users/new", controller.newForm);
+router.post("/admin/users", controller.create);
+
+router.get("/admin/users/:id/edit", controller.editForm);
+router.post("/admin/users/:id", controller.update);
+
+router.post("/admin/users/:id/reset-password", controller.resetPassword);
 
 module.exports = router;
