6fc266020f74f55087a72638c430cddfd3d0e162
angelogds <126365784+angelogds@users.noreply.github.com>
2026-02-10 15:21:21 -0300

    Update dashboard.controller.js

--- modules/dashboard/dashboard.controller.js
+++ modules/dashboard/dashboard.controller.js
@@ -1,14 +1,13 @@
+// modules/dashboard/dashboard.controller.js
 const service = require("./dashboard.service");
 
 function index(req, res) {
   const cards = service.getCards();
-  const blocks = service.getBlocks();
 
   return res.render("dashboard/index", {
-    title: "Painel Principal",
+    title: "Dashboard",
     activeMenu: "dashboard",
     cards,
-    blocks,
   });
 }
 
