633f7ee094273b48d8b47b86e3a426324b559340
angelogds <126365784+angelogds@users.noreply.github.com>
2026-02-17 18:17:32 -0300

    Update dashboard.controller.js

--- modules/dashboard/dashboard.controller.js
+++ modules/dashboard/dashboard.controller.js
@@ -1,10 +1,17 @@
 // modules/dashboard/dashboard.controller.js
 const service = require("./dashboard.service");
+const db = require("../../database/db");
 
 function index(req, res, next) {
   try {
     const cards = service.getCards();
     const plantao = service.getPlantaoAgora();
+
+    const motorRebobCount = db.prepare(`
+      SELECT COUNT(*) as total
+      FROM motores
+      WHERE status = 'ENVIADO_REBOB'
+    `).get().total;
 
     return res.render("dashboard/index", {
       layout: "layout",
@@ -12,7 +19,9 @@ function index(req, res, next) {
       activeMenu: "dashboard",
       cards,
       plantao,
+      motorRebobCount
     });
+
   } catch (err) {
     return next(err);
   }
